a95b86310d3af0042f9c0aa3a62b51375b232133
Gabriel Dulac-Arnold <gabriel@blacksheep.ai>
2018-09-26 18:08:42 +0200

    Working streaming and display of 3x cameras on same hub!

--- bin/gen_dataset.py
+++ bin/gen_dataset.py
@@ -8,7 +8,7 @@ import matplotlib.image as mpimg
 
 from IPython import embed
 
-VIDEO_DEVS = [1, 2, 3]
+VIDEO_DEVS = [0, 1, 2]
 VIDEO_WIDTH = 1280
 VIDEO_HEIGHT = 720
 
@@ -22,7 +22,7 @@ def open_cam_usb(dev, width, height):
     #            'nvvidconv ! appsink').format(dev, width, height)
     gst_str = ('v4l2src device=/dev/video{} ! '
                'image/jpeg,widh=(int){},height=(int){},framerate=10/1,'
-               'format=(string)RGBA ! '
+               'format=(string)RGB ! '
                'jpegdec ! videoconvert ! appsink').format(dev, width, height)
     # gst_str = 'v4l2src ! image/jpeg,width=1280,height=720 ! jpegdec ! videoconvert ! appsink name=sink caps=video/x-raw, format=(string){BGR, GRAY8}; video/x-bayer,format=(string){rggb,bggr,grbg,gbrg}''
     return cv2.VideoCapture(gst_str, cv2.CAP_GSTREAMER)
@@ -58,29 +58,61 @@ def main_stream():
         cap.release()
 
 
-def main():
+# def main():
+#     caps = []
+#     for video_dev in VIDEO_DEVS:
+#         # caps.append(open_cam_usb(video_dev, VIDEO_WIDTH, VIDEO_HEIGHT))
+#         caps.append(cv2.VideoCapture(video_dev))
+#         # cv2.VideoWriter_fourcc('M','J', 'P', 'G')
+
+#     for i in range(60):
+#         for video_dev in VIDEO_DEVS:
+#             # cap = open_cam_usb(video_dev, VIDEO_WIDTH, VIDEO_HEIGHT)
+#             cap = cv2.VideoCapture(video_dev)
+#             # ret, frame =
+#             ret, frame = cap.read()
+#             print(frame)
+#             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+#             # plt.imshow(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+#             # plt.show()
+#             cap.release()
+#             # plt.imshow(frame)
+
+#     # for cap in caps:
+#     #     cap.release()
+
+def display_main():
     caps = []
+
     for video_dev in VIDEO_DEVS:
-        # caps.append(open_cam_usb(video_dev, VIDEO_WIDTH, VIDEO_HEIGHT))
-        caps.append(cv2.VideoCapture(video_dev))
+        cap = open_cam_usb(video_dev, VIDEO_WIDTH, VIDEO_HEIGHT)
+        # cap = cv2.VideoCapture(video_dev)
+        # cap.set(cv2.CAP_PROP_FRAME_WIDTH, VIDEO_WIDTH)
+        # cap.set(cv2.CAP_PROP_FRAME_HEIGHT, VIDEO_HEIGHT)
+        # caps.append(cv2.VideoCapture(video_dev))
+        caps.append(cap)
+
         # cv2.VideoWriter_fourcc('M','J', 'P', 'G')
 
-    for i in range(60):
-        for video_dev in VIDEO_DEVS:
-            # cap = open_cam_usb(video_dev, VIDEO_WIDTH, VIDEO_HEIGHT)
-            cap = cv2.VideoCapture(video_dev)
-            # ret, frame =
+    for i in range(1000):
+        for idx, cap in enumerate(caps):
             ret, frame = cap.read()
-            print(frame)
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            # print(frame)
+            print('.')
+            # time.sleep(0.05)
+            # print(ret, frame)
+            if frame is not None:
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                cv2.imshow('cam{}'.format(idx), frame)
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+            else:
+                print('!')
             # plt.imshow(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
             # plt.show()
-            cap.release()
-            # plt.imshow(frame)
 
-    # for cap in caps:
-    #     cap.release()
-
+    for cap in caps:
+        cap.release()
 
 # def setup():
 #     init_cameras()
@@ -92,4 +124,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main_stream()
+    display_main()
